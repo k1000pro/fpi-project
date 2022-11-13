ff31b25bf8bae169c53d97fb786bac0bf4fcfb01
diegoghoul <61204898+diegoghoul@users.noreply.github.com>
2022-11-12 22:48:44 -0600

    Add files via upload

--- src/components/ModalAnuncio.vue
+++ src/components/ModalAnuncio.vue
@@ -47,53 +47,57 @@
                   <!-- Radio Botton -->
                   <div class="col-8 justify-star">
                     <span >Estado: </span>
-                    <q-radio v-model="estado" val="nuevo" label="Nuevo" />
-                    <q-radio v-model="estado" val="usado" label="Usado" />
+                    <q-radio v-model="nuevo.estado" val="nuevo" label="Nuevo" />
+                    <q-radio v-model="nuevo.estado" val="usado" label="Usado" />
                   </div>
                   <!-- Inputs -->
                   <div class="col-12 col-sm-9">
-                    <q-input  standout="bg-primary text-white" prefix="Marca:" v-model="marca" label="Marca del Telefono *"  :dense="dense" lazy-rules
-                    filled :rules="[ val => val && val.length > 0 || 'Por favor rellene el campo']" />
-                    <q-input standout="bg-primary text-white" prefix="Modelo:" v-model="modelo" label="Modelo del Telefono *"  :dense="dense" lazy-rules
-                      filled :rules="[ val => val && val.length > 0 || 'Por favor rellene el campo']" />
+                    <q-input  standout="bg-primary text-white" prefix="Marca:" v-model="nuevo.marca" label="Marca del Telefono *"  :dense="dense" lazy-rules
+                    filled :rules="[ val => val && val.length > 0 || 'Por favor rellene el campo']" @update:model-value="envioVar" />
+                    <q-input standout="bg-primary text-white" prefix="Modelo:" v-model="nuevo.modelo" label="Modelo del Telefono *"  :dense="dense" lazy-rules
+                      filled :rules="[ val => val && val.length > 0 || 'Por favor rellene el campo']" @update:model-value="envioVar" />
                     <q-input
                           filled
                           standout="bg-primary text-white"
-                          v-model="pantalla"
+                          v-model="nuevo.pantalla"
                           label="Pantalla"
                           prefix="Pantalla:"
                           suffix="Pulgadas"
                           mask="#.#"
                           input-class="text" :dense="dense" lazy-rules
-                          :rules="[ val => val && val.length > 0 || 'Por favor rellene el campo']"
+                          :rules="[ val => val && val.length > 0 || 'Por favor rellene el campo'] "
+                          @update:model-value="envioVar"
                         />
-                      <q-select standout="bg-primary text-white" v-model="sistema" :options="options" prefix="Sistema:" label="Sistema Operativo"  :dense="dense" lazy-rules :rules="[ val => val || 'Por favor rellene el campo']" filled />
-                      <q-input  standout="bg-primary text-white" v-model="rom" label="Almacenamiento interno"  :dense="dense" lazy-rules
-                        filled :rules="[ val => val && val.length > 0 || 'Por favor rellene el campo']"  mask="###" prefix="ROM:" suffix="GB"/>
-                        <q-input  standout="bg-primary text-white" v-model="ram" label="Memoria RAM"  :dense="dense" lazy-rules
-                        filled :rules="[ val => val && val.length > 0 || 'Por favor rellene el campo']"  mask="##" prefix="RAM:" suffix="GB"/>
+                      <q-select standout="bg-primary text-white" v-model="arrSistemas" :options="options" prefix="Sistema:" label="Sistema Operativo"  :dense="dense" lazy-rules :rules="[ val => val || 'Por favor rellene el campo']" filled @update:model-value="envioVar" />
+                      <q-input  standout="bg-primary text-white" v-model="nuevo.rom" label="Almacenamiento interno"  :dense="dense" lazy-rules
+                        filled :rules="[ val => val && val.length > 0 || 'Por favor rellene el campo']"  mask="###" prefix="ROM:" suffix="GB" @update:model-value="envioVar"/>
+                        <q-input  standout="bg-primary text-white" v-model="nuevo.ram" label="Memoria RAM"  :dense="dense" lazy-rules
+                        filled :rules="[ val => val && val.length > 0 || 'Por favor rellene el campo']"  mask="##" prefix="RAM:" suffix="GB" @update:model-value="envioVar"/>
                   </div>
                 </div>
               </div>
               <div class="col-12 col-sm-5 flex ">
                 <div class="row justify-star q-mx-xl q-mb-md">
                   <span class="text-subtitle1 col-12">Titulo breve del anuncio</span>
-                  <q-input class="col-12"  standout="bg-primary text-white"  v-model="titulo"   :dense="dense" lazy-rules
-                    filled :rules="[ val => val && val.length > 0 || 'Por favor rellene el campo']" />
-                    <q-input  standout="bg-primary text-white" prefix="Vendedor:" v-model="vendedor" label="Vendedor del telefono"  :dense="dense" lazy-rules
-                    filled :rules="[ val => val && val.length > 0 || 'Por favor rellene el campo']" />
+                  <q-input class="col-12"  standout="bg-primary text-white"  v-model="nuevo.titulo"   :dense="dense" lazy-rules
+                    filled :rules="[ val => val && val.length > 0 || 'Por favor rellene el campo']"
+                    @update:model-value="envioVar" />
+                    <q-input  standout="bg-primary text-white" prefix="Vendedor:" v-model="nuevo.vendedor" label="Vendedor del telefono"  :dense="dense" lazy-rules
+                    filled :rules="[ val => val && val.length > 0 || 'Por favor rellene el campo']"
+                    @update:model-value="envioVar"/>
                     <q-input
                       filled
-                      v-model="phone"
+                      v-model="nuevo.telefono"
                       label="Numero telefonico"
-                      mask=" #### - ####"
+                      mask=" ####-####"
                       standout="bg-primary text-white"
                       lazy-rules
-                      :rules="[ val => val && val.length > 0 || 'Por favor rellene el campo']"
+                      @update:model-value="envioVar"
+                      :rules="[ val => val && val.length > 8 || 'Por favor rellene el campo']"
                     />
                     <span class="text-subtitle1 col-12">Descripcion</span>
                     <q-input
-                        v-model="descrippcion"
+                        v-model="nuevo.descripcion"
                         filled
                         class="col-12"
                         clearable
@@ -102,6 +106,7 @@
                         color="primary"
                         label="Descripcion del Producto"
                         autogrow
+                        @update:model-value="envioVar"
                       />
                 </div>
               </div>
@@ -118,7 +123,7 @@
                       accept=".jpg, image/*"
                       lazy-rules
                       :rules="[ val => val && val.length > 0 || 'Seleccione las imagenes']"
-                      @update:model-value="obternerURL"
+                      @update:model-value="envioVar ,obternerURL"
                     />
                   </div>
                 </div>
@@ -155,23 +160,24 @@
                         <q-input
                           class=""
                           filled
-                          v-model="price"
+                          v-model="nuevo.precio"
                           :prefix="'$'"
-                          mask="####.##"
+                          reverse-fill-mask
+                          mask="#.##"
                           lazy-rules
+                          @update:model-value="envioVar"
                           :rules="[ val => val && val.length > 0 || 'Por favor rellene el campo']"
                         />
                       </div>
-
                       <div class="col-12 flex justify-center q-mb-md" >
                         <div>
-                          <q-btn icon="close"  label="Cancelar" v-close-popup type="reset" color="primary" flat class="q-mr-md" @click="limpiarFotos" />
-                          <q-btn icon="save" label="Guardas" type="submit" color="primary" />
+                          <q-btn icon="close"  label="Cancelar" v-close-popup type="reset" color="primary" flat class="q-mr-md" @click="limpiarFotos"  />
+                          <q-btn v-if="!envioVar" icon="save" label="Guardar" type="submit" color="primary" />
+                          <q-btn v-if="envioVar" icon="save" label="Guardar" type="submit, reset" color="primary" v-close-popup  />
                         </div>
                       </div>
                     </div>
                   </div>
-
               </div>
             </q-form>
           <!-- Fin -->
@@ -184,27 +190,112 @@
 
 
 <script>
+import { async } from "@firebase/util";
+import { collection, addDoc } from "firebase/firestore";
+import { getStorage, uploadBytes, ref as ref2 } from "firebase/storage";
 import { useQuasar } from 'quasar'
 import { ref, computed } from 'vue'
+import { db } from '../boot/firebase'
 
 export default {
   setup () {
-
-    const descrippcion = ref(null)
     const $q = useQuasar()
-    const estado = ref('nuevo')
-    const marca = ref(null)
-    const pantalla = ref(null)
     const accept = ref(false)
-    const modelo = ref(null)
-    const sistema = ref(null)
-    const rom = ref(null)
-    const ram = ref(null)
-    const titulo = ref(null)
-    const phone = ref(null)
-    const vendedor = ref(null)
     const fotosURL = ref([])
     const fotos = ref()
+    const idArticulo = ref()
+    const arrSistemas = ref()
+    const contaImg = ref(0)
+    const date = new Date()
+    const day = date.getDate()
+    const month = date.getMonth() + 1
+    const year = date.getFullYear()
+    const fecha = (`${year}-${month}-${day}`)
+    const nuevo = ref({
+      estado: 'nuevo', //ya
+      marca: null, //ya
+      modelo: null, //ya
+      pantalla: null, //ya
+      sistema: null, //ya
+      rom: null, //ya
+      ram: null, //ya
+      titulo: null, //ya
+      telefono: null, //ya
+      vendedor: null, //ya
+      precio: null,
+      fecha: null, //ya
+      descripcion: null, //ya
+      fecha: fecha
+    })
+    function limpiarDatos () {
+        nuevo.value.estado = 'nuevo'
+        nuevo.value.marca = null
+        nuevo.value.modelo = null
+        nuevo.value.pantalla = null
+        nuevo.value.sistema = null
+        nuevo.value.rom = null
+        nuevo.value.ram = null
+        nuevo.value.titulo = null
+        nuevo.value.vendedor = null
+        nuevo.value.telefono = null
+        nuevo.value.descripcion = null
+        nuevo.value.precio = null
+        arrSistemas.value = null
+    }
+    function limpiarFotos(){
+      fotosURL.value = [],
+      fotos.value = null
+    }
+    const subirArticulo = async function () {
+      if (nuevo.value.marca && nuevo.value.modelo && nuevo.value.pantalla) {
+        try {
+          nuevo.value.sistema = arrSistemas.value.value
+          const docRef = await addDoc(collection(db, "producto"), nuevo.value);
+          idArticulo.value = docRef.id
+          console.log("Document written with ID: ", docRef.id);
+          subirImagen(idArticulo)
+        } catch (e) {
+          console.error("Error adding document: ", e);
+        }
+      } else {
+          $q.notify({
+            color: 'red-5',
+            textColor: 'white',
+            icon: 'warning',
+            message: 'Necesitas agregar el Estado'
+          })
+      }
+    }
+    const subirImagen = function (idArticulo){
+      console.log(" Estoy subiendo la imagen")
+      const storage = getStorage()
+      fotos.value.forEach( (foto) => {
+        const storageRef = ref2(storage, idArticulo.value + '/' + foto.name )
+        uploadBytes(storageRef, foto).then((snapshot) => {
+          contaImg.value++
+          eslaUltima()
+        });
+      });
+    }
+    function eslaUltima () {
+      console.log("entre al final")
+      if (contaImg.value === fotos.value.length) {
+        $q.notify({
+            color: 'green-4',
+            textColor: 'white',
+            icon: 'cloud_done',
+            message: 'Su articulo se guardo'
+          })
+        limpiarDatos()
+        limpiarFotos()
+      }
+    }
+    const envioVar = computed ( () => {
+      if ( ( (fotos.value != ((null)) ) && nuevo.value.marca != ('' || null)) && (nuevo.value.modelo != ('' || null) ) && (nuevo.value.pantalla != ( '' || null)) && (arrSistemas.value != ('' || null)) && (nuevo.value.rom != ( '' || null)) && (nuevo.value.ram != ( '' || null)) && (nuevo.value.titulo != ( '' || null)) && (nuevo.value.vendedor != ( '' || null)) && (nuevo.value.telefono != ( '' || null))  && (nuevo.value.descripcion != ( '' || null)) && (nuevo.value.precio != ( '' || null)) ) {
+          return true
+        }
+        return false
+    })
 
 
     const obternerURL = computed ( () => {
@@ -216,29 +307,21 @@ export default {
         return fotos
     })
 
-
-
     return {
+      nuevo,
+      envioVar,
+      limpiarDatos,
+      arrSistemas,
+      subirArticulo,
+      limpiarFotos,
       fotos,
       fotosURL,
-      price: ref(null),
       text: ref(null),
-      modelo,
       obternerURL,
-      descrippcion,
-      titulo,
-      rom,
-      ram,
-      estado,
-      marca,
-      pantalla,
       accept,
       dialog: ref(false),
-      sistema,
       dense: ref(false),
-      vendedor,
       slide: ref(1),
-      phone,
       maximizedToggle: ref(true),
       options: [
         {
@@ -254,43 +337,11 @@ export default {
           value: 'harmony'
         }
       ],
-
       onSubmit () {
-
-        if (estado.value == false) {
-          $q.notify({
-            color: 'red-5',
-            textColor: 'white',
-            icon: 'warning',
-            message: 'Necesitas agregar el Estado'
-          })
-        }
-        else {
-          $q.notify({
-            color: 'green-4',
-            textColor: 'white',
-            icon: 'cloud_done',
-            message: 'Submitted'
-          })
-        }
+        subirArticulo()
       },
-
       onReset () {
-        estado.value = 'nuevo'
-        marca.value = null
-        modelo.value = null
-        pantalla.value = null
-        sistema.value = null
-        rom.value = null
-        ram.value = null
-        titulo.value = null
-        vendedor.value = null
-        phone.value = null
-        descrippcion.value = null
-      },
-      limpiarFotos() {
-        fotosURL.value = [],
-        fotos.value = null
+        limpiarDatos()
       }
     }
   }
